a02513476f363300e4d8ecdc578d925484807520
Jakira Dunson <jakiradunson@gmail.com>
2023-05-07 13:27:36 -0500

    challenge

--- Array_notes.js
+++ Array_notes.js
@@ -4,6 +4,42 @@ new Array(): inside the () does not have to b strings, this is called an array f
 [] is the common way to do it
 
 find the amount of elements in the array use the [arrayName.length] method
+.length is not zero base like an array.
+
 to get the last element in the array use the [arrayName.length -1] method
+
+Mutating the array:
+arrayName [location of array] = 'new element'
+
+An entire array can not be replaced
+ */
+
+/*
+Array Methods
+
+ADD ELEMENTS:
+.push() : add elements to the end of an array
+(it is a function that is attached to an array)
+    return the length of the new array
+
+.unshift() : adds elements to the beginning of the array
+
+REMOVE ELEMENTS:
+
+.pop() : remove the last element of array
+    nothing needs to be passed, does not return the length of the new array.
+
+.shift()  : remove the first element of the array
+
+.indexOf('element') : the position the element is in the array
+
+es6 method:
+return true/ false
+    true if the element is in the array
+    false if the element is not in the array
+
+.includes('element') : will check is element is in the array es6 version
+will test using the strict quality
+
 
  */
