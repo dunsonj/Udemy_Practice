3bb8cad93af1dc0cfa1d85c15491cf84a542ded7
Jakira Dunson <jakiradunson@gmail.com>
2023-05-07 12:59:25 -0500

    array

---
+++ Array_notes.js
@@ -0,0 +1,9 @@
+/*ways to write arrays
+[]: bracket notation/ called literal syntax
+new Array(): inside the () does not have to b strings, this is called an array function
+[] is the common way to do it
+
+find the amount of elements in the array use the [arrayName.length] method
+to get the last element in the array use the [arrayName.length -1] method
+
+ */
