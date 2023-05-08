4454aaacd62bb8ed5f64285cf0847bb904b09f62
Jakira Dunson <jakiradunson@gmail.com>
2023-05-07 20:08:45 -0500

    loop notes

---
+++ Loop_Notes.js
@@ -0,0 +1,36 @@
+/*
+for loops:
+meaning:
+for (initial value; logical condition that is evaluated before iteration of the loop; update the counter after each iteration, increase the counter by one)
+
+for (let i = 1; i <= 10; i++){
+return `I love to learn ${i}`;
+}
+
+Looping arrays, break and continuing:
+
+const kira = [
+   'Kira',
+    'Karilen',
+   '22',
+    'software_developer'
+];
+for (let i =0; i <= 4 i++) {
+return kira[0];
+
+another way can be:
+    for (let i =0; i < kira.length ; i++) {
+    console.log(kira[i]);
+}
+ */
+
+const kira = [
+   'Kira',
+    'Karilen',
+   '22',
+    'software_developer'
+];
+
+for (let i =0; i < kira.length ; i++) {
+    console.log(kira[i]);
+}
