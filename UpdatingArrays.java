bc45f14842cfe8198ad12a27fd8d0b9f64e53526
Jakira Dunson <jakiradunson@gmail.com>
2023-08-13 11:35:20 -0500

    updating arrays

--- UpdatingArrays.java
+++ UpdatingArrays.java
@@ -3,7 +3,15 @@ import java.util.Arrays;
 public class UpdatingArrays {
     public static void main(String[] args) {
         String [] menu = {"Expresso", "Ice Coffee", "Macchiato"};
-        String menuString = Arrays.toString(menu);
-        System.out.println(menuString);
+        System.out.println(Arrays.toString(menu));
+//        String menuString = Arrays.toString(menu);
+//        System.out.println(menuString);
+        menu[2] = "Latte";
+        System.out.println(Arrays.toString(menu));
+
+        //to expand an array a new array has to be done
+        String [] newMenu = new String[5];
+
+
     }
 }
