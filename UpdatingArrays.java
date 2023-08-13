b64872ee0c9b4cfac620272bded3bbed0a604475
Jakira Dunson <jakiradunson@gmail.com>
2023-08-13 11:31:14 -0500

    updating arrays

--- UpdatingArrays.java
+++ UpdatingArrays.java
@@ -1,2 +1,9 @@
+import java.util.Arrays;
+
 public class UpdatingArrays {
+    public static void main(String[] args) {
+        String [] menu = {"Expresso", "Ice Coffee", "Macchiato"};
+        String menuString = Arrays.toString(menu);
+        System.out.println(menuString);
+    }
 }
