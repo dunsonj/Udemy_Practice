d2a4c57a3e3459fe6bc6805efe0b17f4973206d8
Jakira Dunson <jakiradunson@gmail.com>
2023-08-13 09:36:26 -0500

    array loop examples

--- ArrayLoopExamples.java
+++ ArrayLoopExamples.java
@@ -29,17 +29,22 @@ public class ArrayLoopExamples {
                     for (int i = 0; i < values.length ; i++){
                         System.out.println(values[i]* 2);
                     }
-
             // Example 5: Capitalize the elements of a string array
             String[] colors = {"red", "green", "blue"};
             // TODO: Use a for loop to capitalize each element in the colors array
-            for (int i = 0; i < colors.length ; i++){
-                System.out.println(colors[i].toUpperCase());
-            }
+                    for (int i = 0; i < colors.length ; i++){
+                        System.out.println(colors[i].toUpperCase());
+                    }
             // Example 6: Find the index of a specific value in an integer array
             int[] data = {4, 7, 4, 1, 4, 9, 4};
             int targetValue = 4;
             // TODO: Use a for loop to find the index of targetValue in the data array
+            for (int i = 0; i < data.length ; i++){
+                System.out.println(data.inde);
+            }
+
+
+
         }
 
 
