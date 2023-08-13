1be242f6c436ee4c63a0bfee90a155de92ef7f4a
Jakira Dunson <jakiradunson@gmail.com>
2023-08-13 12:01:30 -0500

    notes

---
+++ ReferenceTrap.java
@@ -0,0 +1,15 @@
+public class ReferenceTrap {
+    public static void main(String[] args) {
+/**
+ * A variable cannot store an array
+ * It stores a reference that points to it due to that another variable can store a reference that points
+ * at the same array.
+ */
+
+
+
+
+
+
+    }
+}
