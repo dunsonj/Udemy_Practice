5d6aada86c13f369942e57610ba74996b2207215
Jakira Dunson <jakiradunson@gmail.com>
2023-08-13 09:23:45 -0500

    array loop examples

--- ArrayLoopExamples.java
+++ ArrayLoopExamples.java
@@ -4,14 +4,24 @@ public class ArrayLoopExamples {
             // Example 1: Print the elements of an integer array using a for loop
             int[] numbers = {22, 24, 26, 29, 30};
             // TODO: Use a for loop to print each element of the intArray
-
+                    for (int n : numbers){
+                        System.out.println(n);
+                    }
             // Example 2: Print the elements of a string array using a for loop
             String[] strArray = {"apple", "banana", "cherry"};
             // TODO: Use a for loop to print each element of the strArray
-
+                    for (String s : strArray){
+                        System.out.println(s);
+                    }
             // Example 3: Print every second element of an integer array
             int[] grades = {95, 80, 85, 70, 90, 60, 88, 78};
             // TODO: Use a for loop to print every second element of the grades array (step size of 2)
+                    for (int i = 0; i < grades.length ; i+=2){
+                        System.out.println(grades[i]);
+                    }
+            /**
+             * This one would loop through every second element in the array
+             */
 
             // Example 4: Double the elements of an integer array
             int[] values = {3, 5, 7, 9, 11};
