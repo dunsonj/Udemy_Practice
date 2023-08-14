a82520f106df744e9bc48ed639764cbae93740a0
Jakira Dunson <jakiradunson@gmail.com>
2023-08-14 09:43:26 -0500

    2D Arrays

--- TDarrays.java
+++ TDarrays.java
@@ -1,5 +1,17 @@
 public class TDarrays {
     public static void main(String[] args) {
 
+        /**
+         * To create a 2D Array you need to create a tutera, write the data you're storing with two brackets
+         *
+         * Example:
+         *  int [] [] integers = new int [3][4];
+         *  the first bracket is the number of rows and the second bracket is the number of elements per row
+         *
+         */
+
+        int [] [] grades = new int [3][4];
+
+
     }
 }
